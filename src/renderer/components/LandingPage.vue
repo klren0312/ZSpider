c0a573dfb4f9938e4ebad6cda868918fc7b607e7
zzes <kuanglong0312@sina.com>
2019-04-18 16:15:07 +0800

    spider init

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,158 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="left-side">
+        <system-information></system-information>
+      </div>
+
+      <div class="right-side">
+        <input type="text" v-model="mainUrl">
+        <input type="text" v-model="linkRule">
+        <input type="text" v-model="selectAttr">
+        <button @click="start">start</button>
+        <button @click="seeTree">see details</button>
+        <div class="log-list">
+          <p class="log-line" v-for="(v, i) in logs" :key="i">{{v}}</p>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import SystemInformation from './LandingPage/SystemInformation'
+const puppeteer = require('puppeteer')
+let browser = null
+let page = null
+export default {
+  name: 'landing-page',
+  components: { SystemInformation },
+  data () {
+    return {
+      mainUrl: 'http://hf.rent.house365.com/district/',
+      urlArr: [],
+      linkRule: '#JS_listPag > dd > div.info > h3 > a',
+      selectAttr: 'href',
+      logs: [],
+      treeObj: {}
+    }
+  },
+  methods: {
+    async start () {
+      browser = await puppeteer.launch({
+        headless: true
+      })
+      this.writeLog('browser init')
+      page = await browser.newPage()
+      this.writeLog('open new page')
+      this.gotoMain()
+    },
+    async gotoMain () {
+      if (this.mainUrl === '') return
+      try {
+        await page.goto(this.mainUrl)
+      } catch (e) {
+        this.writeLog(e)
+      }
+      this.writeLog(`go to ${this.mainUrl}`)
+      this.writeLog(`use css selector ${this.linkRule}`)
+      let theUrls = await page.$$eval(this.linkRule, function (a) {
+        return a.map(v => {
+          return v.href
+        })
+      })
+      this.treeObj[this.mainUrl] = theUrls
+      this.writeLog(theUrls.join('\n'))
+      for (let i = 2; i < 5; i++) {
+        const pageUrl = `${this.mainUrl}dl_p${i}.html`
+        this.writeLog(`go to ${pageUrl}`)
+        try {
+          await page.goto(pageUrl)
+        } catch (e) {
+          this.writeLog(e)
+        }
+        let urls = await page.$$eval(this.linkRule, a =>
+          a.map(v => {
+            return v.href
+          })
+        )
+        this.treeObj[pageUrl] = urls
+        this.writeLog(urls.join('\n'))
+      }
+      page.close()
+      this.writeLog('关闭页面')
+      page = null
+      browser.close()
+      this.writeLog('关闭浏览器')
+      browser = null
+    },
+    /**
+     * 打印日志
+     */
+    writeLog (v) {
+      if (this.logs.length > 30) {
+        this.logs.shift()
+      }
+      this.logs.push(v)
+    },
+    seeTree () {
+      console.log(this.treeObj)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+  #logo {
+    height: auto;
+    margin-bottom: 20px;
+    width: 420px;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  main > div { flex-basis: 50%; }
+
+  .left-side {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .log-list {
+    max-height: 150px;
+    padding-top: 10px;
+    overflow-y: auto;
+    list-style-type: none;
+    background: #323232;
+    .log-line {
+      margin-bottom: 0;
+      padding-left: 20px;
+      color: antiquewhite;
+    }
+  }
+</style>
---
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="title">Information</div>
+    <div class="items">
+      <div class="item">
+        <div class="name">Memory:</div>
+        <div class="value">{{ usemem | byteFormat }} / {{ totalmem | byteFormat }}</div>
+      </div>
+      <div class="item">
+        <div class="name">Platform:</div>
+        <div class="value">{{ platform }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        platform: require('os').platform(),
+        totalmem: require('os').totalmem(),
+        freemem: require('os').freemem()
+      }
+    },
+    computed: {
+      usemem: function () {
+        return this.totalmem - this.freemem
+      }
+    },
+    filters: {
+      byteFormat: function (size) {
+        if (typeof size !== 'number') {
+          throw new Error('size must be number')
+        }
+        if (size === 0) {
+          return '0 Byte'
+        }
+        var unit = ['Byte', 'KB', 'MB', 'GB', 'TB', 'PB']
+        var pos = 0
+        while (size >= 1024) {
+          size /= 1024
+          ++pos
+        }
+        return size.toFixed(2) + unit[pos]
+      }
+    },
+    mounted () {
+      setTimeout(() => this.refreshMem(), 5000)
+    },
+    methods: {
+      refreshMem () {
+        this.totalmem = require('os').totalmem()
+        this.freemem = require('os').freemem()
+        setTimeout(() => this.refreshMem(), 5000)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    color: #888;
+    font-size: 18px;
+    font-weight: initial;
+    letter-spacing: .25px;
+    margin-top: 10px;
+  }
+
+  .items { margin-top: 8px; }
+
+  .item {
+    display: flex;
+    margin-bottom: 6px;
+  }
+
+  .item .name {
+    color: #6a6a6a;
+    margin-right: 6px;
+  }
+
+  .item .value {
+    color: #35495e;
+    font-weight: bold;
+  }
+</style>
