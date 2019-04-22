c7c96ed8440531a277224d809bbfd262e68c411b
zzes <kuanglong0312@sina.com>
2019-04-22 15:47:13 +0800

    add rule to vuex

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -4,18 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  siteObj: {}
+  siteObj: {},
+  rule: {
+    mainUrl: '',
+    page: 0,
+    contentParams: []
+  }
 }
 
 const mutations = {
   SET_SITE (state, siteObj) {
     state.siteObj = siteObj
+  },
+  SET_RULE (state, rule) {
+    state.rule = rule
   }
 }
 
 const actions = {
   SET_SITE (store, siteObj) {
     store.commit('SET_SITE', siteObj)
+  },
+  SET_RULE (store, rule) {
+    store.commit('SET_RULE', rule)
   }
 }
 export default new Vuex.Store({
