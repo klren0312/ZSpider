e53d04c887553cf75abd282287d8c55887e52b77
klkl <kuanglong0312@sina.com>
2019-11-08 13:02:57 +0800

    fix: ws断线重连

--- src/renderer/wsConnect.js
+++ src/renderer/wsConnect.js
@@ -1,44 +1,43 @@
 import store from './store'
 import EventBus from './utils/EventBus'
-
-let socket = new WebSocket(`${store.state.WSUrl}`)
-let restartNum = 0
-
-socket.onopen = () => {
-  restartNum = 0
-  console.log('socket 连接成功')
-  setInterval(() => {
-    socket.send(JSON.stringify({
-      type: 'heart',
-      msg: 'ping'
-    }))
-  }, 3000)
-  socket.onmessage = msg => {
-    const data = JSON.parse(msg.data)
-    if (data.type === 'heart') {
-      EventBus.$emit('online', true)
+let socket = null
+let interval = null
+function wsConnect () {
+  socket = new WebSocket(`${store.state.WSUrl}`)
+  socket.onopen = () => {
+    console.log('socket 连接成功')
+    interval = setInterval(() => {
+      socket.send(JSON.stringify({
+        type: 'heart',
+        msg: 'ping'
+      }))
+    }, 3000)
+    socket.onmessage = msg => {
+      const data = JSON.parse(msg.data)
+      if (data.type === 'heart') {
+        EventBus.$emit('online', true)
+      }
     }
   }
-}
 
-socket.onclose = () => {
-  EventBus.$emit('online', false)
-  if (restartNum < 50) {
-    restartNum++
-    socket = new WebSocket(`${store.state.WSUrl}`)
-  } else {
-    socket = null
+  socket.onclose = () => {
+    EventBus.$emit('online', false)
+    socket && socket.close()
+    interval && clearInterval(interval)
+    reConnect()
+  }
+
+  socket.onerror = () => {
+    EventBus.$emit('online', false)
+    interval && clearInterval(interval)
   }
 }
 
-socket.onerror = () => {
-  EventBus.$emit('online', false)
-  if (restartNum < 50) {
-    restartNum++
-    socket = new WebSocket(`${store.state.WSUrl}`)
-  } else {
-    socket = null
-  }
+function reConnect () {
+  socket && socket.close()
+  wsConnect()
 }
+
+wsConnect()
 
 export default socket
